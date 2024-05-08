3713ebde9dd8a4765faecb8ccabebd7523e3a3fb
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-05-08 14:10:24 +0630

    log

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
 const express = require("express");
-const { getPosts, createPost, updatePost, getPostById, deletePost } = require("./lib/posts");
+const {
+  getPosts,
+  createPost,
+  updatePost,
+  getPostById,
+  deletePost,
+} = require("./lib/posts");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/api/posts", async (req, res) => {
+  console.log("GET: /posts");
   const posts = await getPosts();
   res.json(posts);
 });
 
 app.post("/api/posts", async (req, res) => {
+  console.log("POST: /posts");
   const post = await createPost(req.body);
   res.json(post);
 });
 
-app.get("/api/posts/:id", async (req,res) => {
-    const post = await getPostById(req.params.id);
-    res.json(post);
-})
+app.get("/api/posts/:id", async (req, res) => {
+  console.log("GET: /posts/" + req.params.id);
 
-app.patch("/api/posts/:id", async (req,res) => {
-    const post = await updatePost(req.params.id, req.body);
-    res.json(post)
+  const post = await getPostById(req.params.id);
+  res.json(post);
 });
 
-app.delete("/api/posts/:id", async (req,res) => {
-    await deletePost(req.params.id);
-    res.json('success');
-})
+app.patch("/api/posts/:id", async (req, res) => {
+  console.log("PATCH: /posts/" + req.params.id);
+
+  const post = await updatePost(req.params.id, req.body);
+  res.json(post);
+});
+
+app.delete("/api/posts/:id", async (req, res) => {
+  console.log("DELETE: /posts/" + req.params.id);
+
+  await deletePost(req.params.id);
+  res.json("success");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
