80e70341e40a054dd780054af5e2ae9923f0526f
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-09-04 16:16:16 +0630

    add authorization res hearder

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.get("/api/posts", async (req, res) => {
   console.log("GET --start: /posts");
-  // console.log(req.headers)
+  console.log(req.headers)
   const posts = await getPosts();
   console.log("GET --end: /posts");
 
