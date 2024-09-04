1ff47398d7e65a9b280fb57ca9cee8035e743e04
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-09-04 15:22:03 +0630

    update: allow all origins

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const {
 } = require("./lib/posts");
 const app = express();
 const PORT = 4000;
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 app.use(express.json());
 
 app.get("/api/posts", async (req, res) => {
